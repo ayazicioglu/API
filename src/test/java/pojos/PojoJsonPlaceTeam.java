56c7797201ada79dd1fcfe9eb16bb560ac8c5e61
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-05-03 17:10:03 +0300

    3.5.23

---
+++ src/test/java/pojos/PojoJsonPlaceTeam.java
@@ -0,0 +1,16 @@
+package pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PojoJsonPlaceTeam {
+
+    private String title;
+    private String body;
+    private int userId;
+    private int id;
+}
